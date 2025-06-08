37e6c0a3bb78c787ab2a6cd090056136ade928c5
StudiesGitHub <sarafnilson@gmail.com>
2025-06-08 10:47:55 -0300

    feat: add controller and basic endpoints

---
+++ src/ProgrammingExercise.API/Controllers/VehiclesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammingExercise.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class VehiclesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult AddVehicle()
+    {
+        return Created();
+    }
+
+    [HttpPut()]
+    public IActionResult UpdateColor()
+    {
+        
+        return NoContent();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll() => Ok();
+
+    [HttpGet("{series}/{number}")]
+    public IActionResult GetVehicle()
+    {
+       
+        return Ok();
+    }
+
+}
